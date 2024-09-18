59101df31944a78fe7dd2270e4205027becb5a9c
Joas Fyllipe <joassolano2908@gmail.com>
2024-09-18 02:17:58 -0300

    Função 01

--- funcoes/Function_01.java
+++ funcoes/Function_01.java
@@ -1,4 +1,4 @@
-package Funções;
+package funcoes;
 
 import java.util.Scanner;
 
@@ -6,7 +6,35 @@ public class Function_01 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        
 
+        System.out.println("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int higher = max(a, b, c);
+
+        showResult(higher);
+
+        sc.close();
     }
+
+    public static int max(int x, int y, int z) {
+        int aux;
+
+        if (x > y && x > z) {
+            aux = x;
+        } else if (y > z && y > x) {
+            aux = y;
+        }
+        else{
+            aux = z;
+        }
+        return aux;
+    }
+
+    public static void showResult(int value){
+        System.out.println("Higher: "+ value);
+    }
+
 }
