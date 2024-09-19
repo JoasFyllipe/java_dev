c20e6b3c031e8832f911903c038799d155b24aca
Joas Fyllipe <joassolano2908@gmail.com>
2024-09-19 00:09:44 -0300

    Exercicio 01

--- exercicios_Classes/Exercicio_01/src/application/Program.java
+++ exercicios_Classes/Exercicio_01/src/application/Program.java
@@ -1,4 +1,27 @@
 package application;
 
+import entities.Retangulo;
+
+import java.awt.*;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter rectangle width and height");
+
+        Retangulo retangulo = new Retangulo();
+
+        retangulo.width = sc.nextDouble();
+        retangulo.height = sc.nextDouble();
+
+        System.out.printf("AREA = "+ String.format("%.2f%n",retangulo.area()));
+        System.out.printf("PERIMETER = "+ String.format("%.2f%n",retangulo.perimeter()));
+        System.out.printf("DIAGONAL = "+ String.format("%.2f%n",retangulo.diagonal()));
+
+    }
 }
--- exercicios_Classes/Exercicio_01/src/entities/Retangulo.java
+++ exercicios_Classes/Exercicio_01/src/entities/Retangulo.java
@@ -1,4 +1,18 @@
 package entities;
 
 public class Retangulo {
+
+    public double width;
+    public double height;
+
+    public double area(){
+        return this.width * this.height;
+    }
+
+    public double perimeter(){
+        return (this.width + this.height) *2;
+    }
+    public double diagonal(){
+        return Math.sqrt(Math.pow(this.width, 2)+(Math.pow(this.height, 2)));
+    }
 }
