42860124c1a34ccc8c593396295b4eb61b043efc
Joas Fyllipe <joassolano2908@gmail.com>
2024-09-21 23:16:45 -0300

    Exercicio de fixação com membros estaticos

--- membros_estaticos/exercicio_fixacao/src/util/CurrencyConverter.java
+++ membros_estaticos/exercicio_fixacao/src/util/CurrencyConverter.java
@@ -1,4 +1,11 @@
 package util;
 
 public class CurrencyConverter {
+
+    public static double priceDollar;
+    public static final double IOF = 0.06;
+
+    public static double vallueDollarGet(double quantityDollarGet){
+        return priceDollar * quantityDollarGet;
+    }
 }
