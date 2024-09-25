a004361ffa3637e162e80667a8cc9425e851740a
Joas Fyllipe <joassolano2908@gmail.com>
2024-09-25 02:01:27 -0300

    Sobrecarga

--- Avancando em POO/Sobrecarga/src/application/Program.java
+++ Avancando em POO/Sobrecarga/src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import entities.Product;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,23 +10,22 @@ public class Program {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        Product p = new Product();
 
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
         String name = sc.nextLine();
         System.out.print("Price: ");
         double price = sc.nextDouble();
-        System.out.println("Quantity in stock: ");
-        int quantity = sc.nextInt();
 
-        Product product = new Product(name, price, quantity);
+        Product product = new Product(name, price);
 
         System.out.println();
         System.out.println("Product data: "+ product);
 
         System.out.println();
         System.out.print("Enter the number of products to be added in stock: ");
-        quantity = sc.nextInt();
+        int quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
--- Avancando em POO/Sobrecarga/src/entities/Product.java
+++ Avancando em POO/Sobrecarga/src/entities/Product.java
@@ -6,10 +6,16 @@ public class Product {
     public double price;
     public int quantity;
 
+    public Product(){
+    }
     public Product(String name, double price, int quantity){
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+    }
+    public Product(String name, double price){
+        this.name = name;
+        this.price = price;
     }
 
     public double totalValuesInStock(){
@@ -21,6 +27,7 @@ public class Product {
     public void removeProducts(int quantity){
         this.quantity -= quantity;
     }
+
     public String toString(){
         return name
             + ", $ "
