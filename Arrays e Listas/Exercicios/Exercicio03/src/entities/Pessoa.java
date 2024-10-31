a00540e72a6cffa49e7b571817a4df73356dc621
Joás Fyllipe <joassolano2908@gmail.com>
2024-10-31 16:29:07 -0300

    Exercicio 03

--- Arrays e Listas/Exercicios/Exercicio03/src/entities/Pessoa.java
+++ Arrays e Listas/Exercicios/Exercicio03/src/entities/Pessoa.java
@@ -16,23 +16,11 @@ public class Pessoa {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public int getIdade() {
         return idade;
     }
 
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
     public double getAltura() {
         return altura;
-    }
-
-    public void setAltura(double altura) {
-        this.altura = altura;
     }
 }
